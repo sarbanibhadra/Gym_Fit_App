883605a8ee87087c69a08489c23a25627b43d35f
sohail <as19adj@herts.ac.uk>
2024-03-15 19:17:01 +0000

    adds tailwind and contact us form

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import './contact.css'
+
+export default function Contact(){
+
+    return(
+        <>
+            <div className="text-3xl font-bold underline">HTMLMLMLHTMLTLTHTL</div>
+        </>
+    )
+
+}
