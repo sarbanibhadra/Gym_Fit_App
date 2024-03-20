2758f65adcbba47d5b2813612467c4078e739ab2
Yusuf Sercan YILDIZ <srcnyildiz@Stars-MacBook-Pro.local>
2024-03-20 06:06:53 +0000

    Bootstrap linked to main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+   document.getElementById('root')
 )
