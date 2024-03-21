7e51ae6f5657b9ba7815a63f1db866b4b6135f3e
Sarbani <sarbani.bhadra@gmail.com>
2024-03-21 19:42:37 +0000

    Adding new set of exercise files

--- src/components/ExerciseSearchForm/index.jsx
+++ src/components/ExerciseSearchForm/index.jsx
@@ -3,7 +3,7 @@ import fb from './fullbody.jpg'
 import './style.css';
 
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
-function ExerciseSearchForm(props) {
+function ExSearchForm(props) {
   return (
     <form className="search">
       <div>
@@ -15,15 +15,19 @@ function ExerciseSearchForm(props) {
           className="form-select" 
           aria-label="Default select example">
           <option defaultValue>Choose your exercise</option>
+          <option value="Back">Back</option>
+          <option value="Chest">Chest</option>
+          <option value="Shoulders">Shoulders</option>
           <option value="Legs">Legs</option>
-          <option value="Core">Core</option>
           <option value="Arms">Arms</option>
+          <option value="Core">Core</option>
         </select>
       </div>
-      <div>  
-        <img src={fb} className="img-fluid" alt="Responsive image" width="30%" height="30%"/>
+      <div className="mr-auto justify-content-center shadow p-3 mb-5 bg-white rounded">  
+        <img src={fb} className="img-fluid " alt="Responsive image" 
+          width="100%" height="60%"/>
       </div>
     </form>
   );
 }
-export default ExerciseSearchForm;
+export default ExSearchForm;
