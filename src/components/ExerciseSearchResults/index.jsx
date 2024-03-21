a4866ce434853e817de885ecbed7d54e356e8ed5
Sarbani <sarbani.bhadra@gmail.com>
2024-03-21 15:56:18 +0000

    Adding card for results

--- src/components/ExerciseSearchResults/index.jsx
+++ src/components/ExerciseSearchResults/index.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import "./style.css";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function SearchResults(props) {
+function ExSearchResults(props) {
   console.log(props.results)
   return (
-    <ul className="list-group search-results"> row
+    <Row xs={1} md={2} className="g-4">
       {props.results.map(result => (
-        <li key={result.id} className="list-group-item"> col-3
-          <p>{result.name}</p>
-          <ul> card-body 
-            {result.instructions.map( obj => (
-              <li key={obj.order}>
-                {obj.description}
-              </li>
-            ))}
-          </ul>
+        <Col key={result.id}>
+        <Card style={{ width: '35rem', height: '18rem' }} >
+          <Card.Body>
+            <Card.Title>{result.name}</Card.Title>
+            <Card.Text>
+              <ul>  
+                {result.instructions.map( obj => (
+                  <li key={obj.order}>
+                  {obj.description}
+                  </li>
+                ))}
+              </ul>
+            </Card.Text>
+          </Card.Body>
           {/* <img alt="Dog" src={result} className="img-fluid" /> */}
-        </li>
+          </Card>
+        </Col>
       ))}
-    </ul>
+    </Row>
   );
 }
 
-export default SearchResults;
+export default ExSearchResults;
